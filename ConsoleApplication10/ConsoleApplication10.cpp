5e0d735f8764ba79f31a129aff293e49ce98eac2
bogor <bogor@DESKTOP-DC6RO5J>
2021-12-16 09:51:02 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication10/ConsoleApplication10.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+#include<Windows.h>
+#include<string>
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int n;
+    string str;
+    cout << "Введите максимальное число символов: " << endl;
+    cin >> n;
+    cout << "Введите предложение: " << endl;
+    cin >> str;
+    if (n >= str.size())
+    {
+        for (int i = 0; i < str.size(); i++)
+        {
+            if (str[i] == '.') { str.insert(i++, "."); str.insert(i++, "."); }
+        }
+        cout << str << endl;
+    }
+    else { cout << "Неверное количество символов" << endl; }
+    return 0;
+}
